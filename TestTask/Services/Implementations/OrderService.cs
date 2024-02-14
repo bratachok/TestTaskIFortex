068a54e799cfcc7a9e936bc823758e4ff4b3a25b
user <user@DESKTOP-KGF6SCR>
2024-02-14 19:28:47 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext context;
+        public UserService(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            User result = context.Users.Where(p => p.Id == context.Orders
+            .GroupBy(o => o.UserId)
+            .OrderByDescending(o => o.Count())
+            .First().Key).First();
+
+            return result;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            List<User> result = context.Users.Where(p => p.Status == Enums.UserStatus.Inactive).ToList();
+            return result;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Data;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext context;
+        public OrderService(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        public async Task<Order> GetOrder()
+        {
+            Order result = context.Orders.Where(p => p.Price * p.Quantity == context.Orders.Max(o => o.Price * o.Quantity)).FirstOrDefault();
+            return result;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            List<Order> result = context.Orders.Where(p => p.Quantity >= 10).ToList();
+            return result;
+        }
+    }
+}
